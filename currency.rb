5155154f92b7314d9911cc0f77ab7a547f484771
jessejkwk <janderson@nyatoday.com>
2018-06-19 19:43:31 +0000

    Done.

--- currency.rb
+++ currency.rb
@@ -1,1 +1,14 @@
 # Write your cod here
+
+def usd_to_eur(money)
+  euro = money * 0.8638
+  puts "Ok! You have #{money} euro when submitting #{euro} euros"
+end
+
+def eur_to_usd(euro)
+  usd = euro / 0.8638
+  puts "Ok! You have #{euro} euros when submitting #{usd} euros"
+  
+end
+
+eur_to_usd(10)
